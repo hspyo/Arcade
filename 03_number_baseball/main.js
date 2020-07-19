d67257540923093b451456a82dd7eb14871363e8
Pyo <olok95@gmail.com>
2020-07-19 22:44:18 +0900

    Baseball refactoring

--- 03_number_baseball/main.js
+++ 03_number_baseball/main.js
@@ -1,17 +1,18 @@
 var body = document.body;
-
 var nums;
-var numArr
+var numArray;
+
 
 function pickNums() {
-  var nums = [1,2,3,4,5,6,7,8,9];
-  var numArr = [];
+  nums = [1,2,3,4,5,6,7,8,9];
+  numArray = [];
+  
   for (var i=0; i<4; i += 1) {
-    var result = nums.splice(Math.ceil(Math.random() * (9 - i)), 1)[0]; // splice(위치, 개수) : 위치로부터 개수만큼 배열에서 뽑음.
-    numArr.push(result); // push : 1,2,3 ... 순서대로 배열에 넣는 것.               <--> unshift : 0번 인덱스에 추가 
+    var choicedNum = nums.splice(Math.floor(Math.random() * (9 - i)), 1)[0]; // splice(위치, 개수) : 위치로부터 개수만큼 배열에서 뽑음.
+    numArray.push(choicedNum); // push : 1,2,3 ... 순서대로 배열에 넣는 것.               <--> unshift : 0번 인덱스에 추가
   }                        //pop : 배열 마지막 것부터 하나씩 뽑는 것.                 <--> shift : 처음부터 뽑는 것.
+    
 }
-
 pickNums();
 var result = document.createElement('h1');
 body.append(result);
@@ -27,28 +28,30 @@ var wrong = 0;
 form.addEventListener('submit', function (e) { //비동기 (언제 실행될지 모름)
   e.preventDefault(); // submit의 기본동작인 새로고침을 방지.
   var answer = input.value;
-  if (answer === numArr.join('')) {
+  if (answer === numArray.join('')) {
   result.textContent = '홈런';
   input.value = '';
   input.focus();
   pickNums();
   wrong = 0;
-  } else{
+  }
+  else{
     var answerArr = answer.split('');
     var strike = 0;
     var ball = 0;
     wrong += 1;
     if (wrong > 10) {
-      result.textContent = "10회 초과! 실패. 정답: " + numArr.join(',') + "입니다.";
+      result.textContent = "10회 초과! 실패. 정답: " + numArray.join('') + "입니다.";
       input.value = '';
       input.focus();
       pickNums();
       wrong = 0;
     } else {
     for (var i=0; i < 3; i += 1) {
-      if (Number(answerArr[i]) === numArr[i]) { //같은 자리인지 확인
+      if (Number(answerArr[i]) === numArray[i]) { //같은 자리인지 확인
+        console.log('같은 자리면');
         strike += 1;
-      }else if (numArr.indexOf(Number(answerArr)) > -1) { // 같은 자리는 아니지만 수잦가 겹치는지 확인
+      }else if (numArray.indexOf(Number(answerArr)) > -1) { // 같은 자리는 아니지만 수잦가 겹치는지 확인
         ball += 1;
       }
     }
